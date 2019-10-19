75eef69af7225d0d94e44d6f926c6b0bcfc85972
name <email>
2019-10-19 17:58:54 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/example/weatherforecast/android/db/County.java
@@ -0,0 +1,52 @@
+package com.example.weatherforecast.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/*
+ * @name WeatherForecast
+ * @class name:com.example.weatherforecast.android.db
+ * @anthor:59266
+ * @time 2019/10/19 17:51
+ */
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
